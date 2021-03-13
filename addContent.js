ec354ca4991d600d825eb43a207c5af139622493
ottovenliu <76770643+ottovenliu@users.noreply.github.com>
2021-03-13 09:01:36 +0800

    upload0313

--- addContent.js
+++ addContent.js
@@ -2,7 +2,8 @@ window.onload = function addImg() {
     for (i = 0; i < lessShow; i++) {
         var imageSources = items[i][1];
         var img = document.createElement("img");
-        img.style.width = "100%";
+        img.style.maxWidth = "100%";
+        img.style.maxHeight = "260px"
         // img.style.height = "100%";
         img.src = imageSources;
         document.getElementById(`item_${i}`).appendChild(img)
--- createDiv.js
+++ createDiv.js
@@ -3,17 +3,19 @@ var parent = document.getElementById('contentArea');
 console.log(parent)
 for (var i = 0; i < lessShow; i++) {
     var newDiv = document.createElement('div');
+    var textBox = document.createElement('div')
     var textNode = document.createTextNode(items[i][0]);
     // var imageSources = items[i][1];
     // var img = document.createElement("img");
-    newDiv.appendChild(textNode);
+    textBox.appendChild(textNode);
+    newDiv.appendChild(textBox)
     newDiv.setAttribute("class", "item")
     newDiv.setAttribute("id", `item_${i}`)
     // img.src = imageSources;
     // document.getElementById(`item_${i}`).appendChild(img)
     // append the new div to the parent
 
-    newDiv.appendChild(textNode);
+    // newDiv.appendChild(textNode);
     parent.appendChild(newDiv);
 }
 
